958ab6189d4036cd490b8304e9564ada3f74149f
kanoqwq <kanoqwq@qq.com>
2025-04-15 20:31:31 +0800

    feat：流量设置，修复样式

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "2.2.5"
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

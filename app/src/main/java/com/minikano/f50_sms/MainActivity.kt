642189ff25cd804493c3edcfc3aefa946fbc06a4
kanoqwq <kanoqwq@qq.com>
2025-04-17 10:30:39 +0800

    fix:取消注册jsInterface

--- app/src/main/java/com/minikano/f50_sms/MainActivity.kt
+++ app/src/main/java/com/minikano/f50_sms/MainActivity.kt
@@ -62,7 +62,7 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        webView.addJavascriptInterface(JSInterface(), "KANO_INTERFACE")
+//        webView.addJavascriptInterface(JSInterface(), "KANO_INTERFACE")
         showLoadingDialog()
         // 异步处理 IP 获取
         Thread {

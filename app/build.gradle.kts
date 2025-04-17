695661bb8e4686304ddc0a79b548750bd23c6210
kanoqwq <kanoqwq@qq.com>
2025-04-18 00:07:12 +0800

    feat:u30air 手机端电量显示，修复样式

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "2.3.5"
+        versionName = "2.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

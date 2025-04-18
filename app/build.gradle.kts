2e8a8befee92877d120711f533cc479fd8bdaa0b
kanoqwq <kanoqwq@qq.com>
2025-04-18 14:48:45 +0800

    fix:样式，修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "2.3.9"
+        versionName = "2.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

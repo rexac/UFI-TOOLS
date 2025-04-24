ea0d26c2b47db4e5d58f9db1efe09ebf829b2d30
minikano <kanoqwq@qq.com>
2025-04-24 12:36:55 +0800

    feat:主题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "2.6.0"
+        versionName = "2.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

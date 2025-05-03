76eaa2df618afef1a6dda0cf8159d393dbf6f9e4
minikano <kanoqwq@qq.com>
2025-05-03 23:25:55 +0800

    fix:版本号修改为2.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "2.7.0"
+        versionName = "2.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

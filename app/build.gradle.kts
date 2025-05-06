51c9ae7cb0d106bfa27ba7037bcc9d1214a54f47
minikano <kanoqwq@qq.com>
2025-05-06 16:05:12 +0800

    feat:漫游开关，赞赏作者功能(非恰饭)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "2.7.1"
+        versionName = "2.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

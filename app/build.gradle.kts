552171d0dd70eab47938b05030886567ca0a4b60
minikano <kanoqwq@qq.com>
2025-05-08 00:36:15 +0800

    feat:抽屉

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "2.7.3"
+        versionName = "2.7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

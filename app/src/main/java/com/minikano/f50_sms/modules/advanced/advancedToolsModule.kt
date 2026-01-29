84f9978a1acb5bb0db1176139c84ab89d9e10e35
minikano <kanoqwq@qq.com>
2026-01-29 21:14:03 +0800

    feat:v3.9.4:短信转发新增bark转发

--- app/src/main/java/com/minikano/f50_sms/modules/advanced/advancedToolsModule.kt
+++ app/src/main/java/com/minikano/f50_sms/modules/advanced/advancedToolsModule.kt
@@ -370,7 +370,7 @@ fun Route.advancedToolsModule(context: Context, targetServerIP: String) {
             }
 
             val text = json.optString("command", "").trim()
-            var timeout = json.optInt("timeout",100*1000)
+            var timeout = json.optInt("timeout",100 * 1000)
 
             timeout = if(timeout > 100 * 1000) {
                 KanoLog.d(TAG, "timeout大于100*1000ms，将按照100s计算")
